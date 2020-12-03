1268147fb70b708c2ab3243f3dab7582785e2115
Kliment Serafimov <kliment@mit.edu>
2020-12-02 18:52:52 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Bitvector
+{private:
+    int bitvector;
+public:
+    Bitvector(int _bitvector)
+    {
+        bitvector = _bitvector;
+    }
+    //dude it's fine since you haven't done this sober!!!
+
+};
+
+int main() {
+
+    //parameters:
+    //n - the number of input variables
+    //L - the language, given as a subset of {{0, 1}^2 -> {0, 1}}
+    //train_set - set of input output examples, given as a subset of {({0, 1}^n, {0, 1})}
+    //goal: synthesize a program in L
+
+    for(int i = 0; i < 16; i++)
+    {
+
+    }
+
+    return 0;
+}
